2504093a609cf90d8d643a315c0cc11b6879738b
IsadoraAbreuu <isaabreucorrea@gmail.com>
2025-10-14 21:57:41 -0300

    jogo da velha

--- jogo-velha/src/components/Tabela.jsx
+++ jogo-velha/src/components/Tabela.jsx
@@ -1,13 +1,20 @@
 import Quadrado from "./Quadrado"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import confetti from "canvas-confetti"
+
 
 export default function Tabela() {
 
-    const [quadrados, setQuadrados] = useState(Array(9).fill(" "))
+    //define variaves e guarda cada quadrado em uma lista de 9 posições, todas começando vazias
+    const [quadrados, setQuadrados] = useState(Array(9).fill(""))
+    //define as variaveis para controlar a vez de cada jogador ("O" ou "X")
     const [xProximo, setXProximo] = useState(true)
+    //define as variaveis para temas
+    const [tema, setTema] = useState("azul")
 
+    //função para alternar as jogadas entre "O" e "X"
     function handleClick(i) {
-        if (quadrados[i] != " "|| calcularVencedor(quadrados) === null) {
+        if (quadrados[i] !== "" || calcularVencedor(quadrados)) {
             return;
         }
         
@@ -24,7 +31,26 @@ export default function Tabela() {
         setXProximo(!xProximo);
     }
 
+    //função para recomeçar o jogo ao clicar no botao de recomecar
+    function Recomecar() {
+        setQuadrados(Array(9).fill(""));
+        setXProximo(true);
+    }
+
+    //condição para mostar o vencedor ou o proximo a jogar
     const vencedor = calcularVencedor(quadrados);
+
+    useEffect(() => {
+        if (vencedor) {
+            confetti({
+                particleCount: 200,
+                spread: 80,
+                origin: { y: 0.6 },
+            });
+            alert('O ' + vencedor + ' ganhou!');
+        }
+    }, [vencedor]); //só dispara quando o vencedor muda
+
     let status;
     if (vencedor) {
         status = "Vencedor: " + vencedor;
@@ -32,10 +58,9 @@ export default function Tabela() {
         status = "Próximo a jogar: " + (xProximo ? "X" : "O");
     }
 
-
     return (
         <>
-        <section className="containerJogo">
+        <section className={`containerJogo tema-${tema}`}>
             <h2>Jogo da</h2>
             <h1>Velha</h1>
             <div className="containerVelha">
@@ -55,16 +80,22 @@ export default function Tabela() {
                     <Quadrado value={quadrados[8]} onQuadradoClick = {() => handleClick(8)}/>
                 </div>
                 <div className="containerProximo">
-                    <h3>Vencedor: {vencedor}</h3>
-                    <h3>Próximo a jogar: {}</h3>
-                    <button type="submit" onClick={Recomecar}>Recomeçar</button>
+                    <h3>Vencedor: {vencedor || "—"}</h3>
+                    <h3>Próximo a jogar: {xProximo ? "X" : "O"}</h3>
+                    <button onClick={Recomecar}>Recomeçar</button>
                 </div>
+                <div className="temas">
+                        <button className="btnAzul" onClick={() => setTema("azul")}>Azul</button>
+                        <button className="btnRosa" onClick={() => setTema("rosa")}>Rosa</button>
+                        <button className="btnVerde" onClick={() => setTema("verde")}>Verde</button>
+                    </div>
             </div>
         </section>
         </>
     )
 }
 
+//função para calcular o vencedor com as condições de um jogo da velha normal 
 function calcularVencedor(quadrados) {
       const linhas = [
         [0, 1, 2],
@@ -85,6 +116,4 @@ function calcularVencedor(quadrados) {
   return null;
 }
 
-function Recomecar() {
-    // codigo para recomeçar
-}
+
